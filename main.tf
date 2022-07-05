80c27649e15041f3133eea391f2f9cfaf6af2fa6
mohdzakir12 <zakir.mohd@synectiks.com>
2022-07-05 14:16:39 +0530

    added subnet group

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_rds_cluster" "rds-cluster" {
   engine              = var.engine
   engine_version      = var.engine_version
   #availability_zones  = var.availability_zones
-  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1e"]
   database_name       = var.database_name
   master_username     = var.master_username
   master_password     = var.master_password
@@ -17,4 +17,10 @@ resource "aws_rds_cluster" "rds-cluster" {
   iops                = var.iops
   skip_final_snapshot = var.skip_final_snapshot
   backup_retention_period = var.backup_retention_period
+  db_subnet_group_name = aws_db_subnet_group.subnetgroup.name
+}
+
+resource "aws_db_subnet_group" "subnetgroup" {
+  name       = "main"
+  subnet_ids = ["subnet-07630e0e427e128cd", "subnet-0d55dad9d07e7b7ad", "subnet-0f74ce01b4f309938"]
 }
